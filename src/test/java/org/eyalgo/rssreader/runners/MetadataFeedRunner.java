570f3ec997e2cb62683b5df3c54e6a6290901ace
Eyal Golan <egolan74@gmail.com>
2014-05-28 00:42:09 +0300

    add ROME dependency to pom. create a small runner to see how reading a feed

---
+++ src/test/java/org/eyalgo/rssreader/runners/MetadataFeedRunner.java
@@ -0,0 +1,33 @@
+package org.eyalgo.rssreader.runners;
+
+import java.io.File;
+import java.net.URL;
+import java.util.List;
+
+import com.sun.syndication.feed.synd.SyndEntry;
+import com.sun.syndication.feed.synd.SyndFeed;
+import com.sun.syndication.io.SyndFeedInput;
+
+public class MetadataFeedRunner {
+
+    public MetadataFeedRunner() {
+    }
+    
+    private void printFeed() throws Exception {
+	SyndFeedInput input = new SyndFeedInput();
+	URL url = this.getClass().getResource("cnn.small");
+	System.out.println(url);
+	File file = new File(url.toURI());
+	SyndFeed feed = input.build(file);
+	@SuppressWarnings("unchecked")
+	List<SyndEntry> entries = feed.getEntries();
+	for (SyndEntry entry : entries) {
+	    System.out.println(entry.getTitle());
+	}
+    }
+
+    public static void main(String[] args) throws Exception {
+	new MetadataFeedRunner().printFeed();
+    }
+
+}
