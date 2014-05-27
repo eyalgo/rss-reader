0e76f220e51fd74bd7bcce0ae2412b5af85fc016
Eyal Golan <egolan74@gmail.com>
2014-05-28 01:39:46 +0300

    add test data for outbrain (different RSS format). runner imporved

--- src/test/java/com/eyalgo/rssreader/runners/MetadataFeedRunner.java
+++ src/test/java/com/eyalgo/rssreader/runners/MetadataFeedRunner.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.net.URL;
 import java.util.List;
 
+import com.sun.syndication.feed.synd.SyndContent;
 import com.sun.syndication.feed.synd.SyndEntry;
 import com.sun.syndication.feed.synd.SyndFeed;
 import com.sun.syndication.io.SyndFeedInput;
@@ -12,10 +13,15 @@ public class MetadataFeedRunner {
 
     public MetadataFeedRunner() {
     }
-    
+
     private void printFeed() throws Exception {
+	printEntries("cnn.small.xml");
+	printEntries("outbrain-small.xml");
+    }
+    
+    private void printEntries(String filePath) throws Exception {
 	SyndFeedInput input = new SyndFeedInput();
-	URL url = this.getClass().getResource("cnn.small");
+	URL url = this.getClass().getResource(filePath);
 	System.out.println(url);
 	File file = new File(url.toURI());
 	SyndFeed feed = input.build(file);
@@ -25,6 +31,12 @@ public class MetadataFeedRunner {
 	    System.out.println(entry.getTitle());
 	    System.out.println(entry.getLink());
 	    System.out.println(entry.getPublishedDate());
+	    @SuppressWarnings("unchecked")
+	    List<SyndContent> contents = entry.getContents();
+	    for (SyndContent content : contents) {
+		System.out.println(content.getValue());
+	    }
+//	    System.out.println(contents);
 	    System.out.println();
 	}
     }
